254f3045bebc560020dceab4eff375808614f66d
Katya <ekaterina.radionova@skoltech.ru>
2021-03-04 17:26:30 +0300

    segmentation_baselines

---
+++ segmentation_baselines/helper.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_img_array(img_array, ncol=3):
+    nrow = len(img_array) // ncol
+
+    f, plots = plt.subplots(nrow, ncol, sharex='all', sharey='all', figsize=(ncol * 4, nrow * 4))
+
+    for i in range(len(img_array)):
+        plots[i // ncol, i % ncol]
+        plots[i // ncol, i % ncol].imshow(img_array[i])
+
+from functools import reduce
+def plot_side_by_side(img_arrays):
+    flatten_list = reduce(lambda x,y: x+y, zip(*img_arrays))
+
+    plot_img_array(np.array(flatten_list), ncol=len(img_arrays))
+
+def masks_to_colorimg(masks):
+    colors = np.asarray([(0, 0, 0), (0, 0, 0), (255, 255, 255)])
+    #np.asarray([(201, 58, 64), (101, 172, 228), (0, 100, 132)]) #, (101, 172, 228),(56, 34, 132), (160, 194, 56)])
+
+    colorimg = np.ones((masks.shape[1], masks.shape[2], 3), dtype=np.float32) * 255
+    channels, height, width = masks.shape
+
+    for y in range(height):
+        for x in range(width):
+            selected_colors = colors[masks[:,y,x] > 0.5]
+
+            if len(selected_colors) > 0:
+                colorimg[y,x,:] = np.mean(selected_colors, axis=0)
+
+    return colorimg.astype(np.uint8)
